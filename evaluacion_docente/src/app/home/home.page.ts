da8d304816a8670aaa43845b9473e1e27bd61511
OzielLM <oziel_bear@icloud.com>
2024-05-13 10:54:50 -0600

    Modificación de la pagina inicial
    Se modifico la pagina inicial para que sea el inicio de sesión para el administrador y los alumnos

--- evaluacion_docente/src/app/home/home.page.ts
+++ evaluacion_docente/src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from '@ionic/angular'; // Importar NavController
+import { NavController, AlertController } from '@ionic/angular'; // Importar NavController
 
 @Component({
   selector: 'app-home',
@@ -8,9 +8,54 @@ import { NavController } from '@ionic/angular'; // Importar NavController
 })
 export class HomePage {
 
-  constructor(private navCtrl: NavController) {}  // Inyectar NavController
+  constructor(private navCtrl: NavController, private alertController: AlertController) {}  // Inyectar NavController
 
   goToMaterias() {
     this.navCtrl.navigateForward('/materias');
+  }
+
+  customCounterFormatter(inputLength: number, maxLength: number) {
+    return `${maxLength - inputLength} characters remaining`;
+  }
+
+  public alertButtons = [
+    {
+      text: 'Ver Materias',
+      handler: () => {
+        this.goToMaterias(); 
+      }
+    }
+  ]
+  public alertInputs = [
+    {
+      placeholder: 'Matricula (max 5 caracteres)',
+      attributes: {
+        maxlength: 5,
+      },
+    }
+  ];
+
+  login() {
+    const usuarioInput = document.getElementById('usuarioInput') as HTMLIonInputElement;
+    const contrasenaInput = document.getElementById('contrasenaInput') as HTMLIonInputElement;
+
+    const nombreUsuario = usuarioInput.value;
+    const contrasena = contrasenaInput.value;
+
+    if(nombreUsuario == "Admin" && contrasena == "admin123"){
+      this.goToMaterias();
+    }else{
+      this.mostrarAlertaError();
+    };
+  }
+
+  async mostrarAlertaError() {
+    const alert = await this.alertController.create({
+      header: 'Error de inicio de sesión',
+      message: 'Usted no esta autorizado para entrar',
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 }
